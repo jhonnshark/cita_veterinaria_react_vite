ff3c0de3de4f0bb770096475729d4d9e2188a0b9
jhonnshark <jhonnshark@gmail.com>
2023-12-31 02:16:37 -0500

    No fue mucho trabajo pero fue un trabajo onesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 const Header = () => {
-	return (
-		<>
-			<h1 className="font-black  text-4xl text-center md:w-3/1 mx-auto">
-				Seguimiento Pacientes <span className="text-indigo-600">Veterinaria</span>
-			</h1>
-		</>
-	);
+  return (
+    <>
+      <h1 className="font-black  text-4xl text-center md:w-3/1 mx-auto">
+        Seguimiento Pacientes {''} Con Github {''}
+        <span className="text-indigo-600">Veterinaria</span>
+      </h1>
+    </>
+  );
 };
 
 export default Header;
